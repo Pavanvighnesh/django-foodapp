a98b30d7943ccd360407432239759cd5ad69230d
pavan <vighneshkumar2821gmail.com>
2024-03-27 14:23:37 +0530

    second commit

---
+++ Desktop/django_foodapp/food_proj/food/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from .models import item
+from .forms import item_form
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
+from .forms import registration_form
+
+# Create your views here.
+def home(request):
+    item_list=item.objects.all()
+    context={
+        'items':item_list
+    }
+    return render(request,'home.html',context)
+
+
+def details(request,item_id):
+    snack=item.objects.get(pk=item_id)
+    context={
+        'snack':snack
+    }
+
+    return render(request,'details.html',context)
+
+def create_item(request):
+    form=item_form(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('food:home')
+    
+    return render(request,'item_create.html',{'form':form})
+
+
+def update(request,id):
+    itm=item.objects.get(id=id)
+    form=item_form(request.POST or None,instance=itm)
+
+    if form.is_valid():
+        form.save()
+        return redirect('food:home')
+    return render(request,'item_create.html',{'form':form,'itm':itm})
+
+def delete(request,pk):
+    itm=item.objects.get(id=pk)
+    if request.method=='POST':
+     itm.delete()
+     return redirect('food:home')
+    return render(request,'delete.html',{'itm':itm})
+
+def register(request):
+    if request.method=='POST':
+     form=registration_form(request.POST)
+     if form.is_valid():
+        form.save()
+        username=form.cleaned_data.get('username')
+        messages.success(request,f'Welcome {username}... You successfully registered')
+        return redirect('food:home')
+    else:
+       form=registration_form() 
+
+    return render(request,'register.html',{'form':form})   
+
+   
